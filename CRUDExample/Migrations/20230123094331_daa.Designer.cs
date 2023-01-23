a449be44b1bfce570051d3ac648768e82105dd30
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-01-23 12:47:11 +0300

    modelBuilder.Entity<Person>().HasData(personList);

--- CRUDExample/Migrations/20230123094331_daa.Designer.cs
+++ CRUDExample/Migrations/20230123094331_daa.Designer.cs
@@ -4,6 +4,7 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRUDExample.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230123094331_daa")]
+    partial class daa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
